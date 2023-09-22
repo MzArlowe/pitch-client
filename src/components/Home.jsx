f7eb03c426dec895bf3d30537d7b7a610e863092
Edwin Torres <me@edwintorres.dev>
2023-09-22 03:05:27 -0400

    Expanding the homepage to what it used to look like with the new design.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,37 @@ import './Home.css';
 export default function Home() {
 	return (
 		<>
-			<div className='home'></div>
+			<div className='home'>
+				<iframe
+					id='inlineFrameExample'
+					title='Inline Frame Example'
+					width='300'
+					height='200'
+					src="https://www.openstreetmap.org/export/embed.html?bbox=-86.62788391113281%2C39.52787769468346%2C-85.80940246582033%2C40.031294855540054&layer=mapnik"
+				></iframe>
+			</div>
+
+			<div id='media'>
+				<h2>Media</h2>
+			</div>
+
+			<div id='uploads'>
+				<form
+					method='post'
+					action='/upload'
+				>
+					{/* <input type="text" value="Click here and press tab to test keyboard interaction."></input> */}
+					<label class='file'>
+						Drop a file or click to select one
+						<input
+							type='file'
+							multiple
+						></input>
+						{/* use multiple, even if it’s not allowed, to be able to show an info text */}
+					</label>
+					<button type='submit'>Upload</button>
+				</form>
+			</div>
 		</>
 	);
 }
