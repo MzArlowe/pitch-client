e6e0938112a977174704b0658277d09f6c59fc32
Edwin Torres <me@edwintorres.dev>
2023-09-22 02:03:19 -0400

    Re-wrote the App to use React Components and JSX.

---
+++ src/components/MenuData.js
@@ -0,0 +1,31 @@
+export const MenuData = [
+	{
+		title: 'Home',
+		url: '/',
+		cName: 'nav-links',
+		icon: 'fa-solid fa-house-user',
+	},
+	{
+		title: 'About',
+		url: '/About',
+		cName: 'nav-links',
+		icon: 'fa-solid fa-circle-info',
+	},
+	{
+		title: 'Service',
+		url: '/Service',
+		cName: 'nav-links',
+		icon: 'fa-solid fa-briefcase',
+	},
+	{
+		title: 'Contact',
+		url: '/Contact',
+		cName: 'nav-links',
+		icon: 'fa-solid fa-address-book',
+	},
+	{
+		title: 'Sign Up',
+		url: '/SignUp',
+		cName: 'nav-links-mobile',
+	},
+];
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { MenuData } from './MenuData';
+import './Navbar.css';
+
+export default function Navbar() {
+	const [clicked, setClicked] = useState(false);
+
+	const handleClick = () => {
+		setClicked(!clicked);
+	};
+
+	return (
+		<nav className={`navbar-items ${clicked ? 'active' : ''}`}>
+			<h1 className='logo'>
+				<i className='fab fa-react'></i> Pitch
+			</h1>
+
+			<div
+				className='menu-icons'
+				onClick={handleClick}
+			>
+				<i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+			</div>
+
+			<ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+				{MenuData.map((item, index) => (
+					<li key={index}>
+						<a
+							href={item.url}
+							className={item.cName}
+						>
+							<i className={item.icon}></i>
+							{item.title}
+						</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+}
