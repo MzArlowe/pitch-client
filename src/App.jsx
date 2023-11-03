a61b4bac734e97e38ae7adb97d634b8737f62b39
Benjamin Marlowe <bhm727@yahoo.com>
2023-11-02 22:11:52 -0400

    SignUp and SignIn
    Created the first two cards for Onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+// import Dashboard from '.components/Dashboard';
 import About from './components/About';
 import Footer from './components/Footer';
 import HTTP404 from './components/HTTP404';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home />,
 	},
+	{
+		path: 'SignIn',
+		element: <SignIn />,
+	},
+
+	{
+		path: 'SignUp',
+		element: <SignUp />,
+	},
+
 	{
 		path: 'About',
 		element: <About />,
