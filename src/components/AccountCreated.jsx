5fc1bec4cf7d5599043f332c549a2cf682d0c253
Lenny3000 <91618917+Lenny3000@users.noreply.github.com>
2023-12-13 21:50:33 -0500

    Update AccountCreated.jsx

--- src/components/AccountCreated.jsx
+++ src/components/AccountCreated.jsx
@@ -1,23 +1,32 @@
 import './AccountCreated.css';
-import { setUserType } from './NavbarData';
+// import { setUserType } from './NavbarData';
 
 export default function AccountCreated() {
+	const progress = 0; // Set initial value (0-100).
+	let progressDiv = progress;
+	if (progress < 2) {
+		progressDiv = 2;
+	}
 	return (
 		<div>
 			<h1>Funder Account Created!</h1>
-			<p>
-				If you ever need to update of change your information or project
-				details, it will be in your profile settings
-			</p>
-			<p>Let's check out your dashboard</p>
-
 			<div>
-				<a
-					className='button'
-					href='/UserOne'
-				>
-					<button>Next</button>
-				</a>
+				<h6>{progress}%</h6>
+
+				<p>
+					If you ever need to update of change your information or project
+					details, it will be in your profile settings
+				</p>
+				<p>Let's check out your dashboard</p>
+
+				<div>
+					<a
+						className='button'
+						href='/UserOne'
+					>
+						<button>Next</button>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
