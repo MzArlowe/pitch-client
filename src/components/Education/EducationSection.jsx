92bd04fde346bba3ebc77062f8f6cf7df7254578
Benjamin Marlowe <bhm727@yahoo.com>
2024-10-17 20:59:37 -0400

    Reworking Education Section

--- src/components/Education/EducationSection.jsx
+++ src/components/Education/EducationSection.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 export default function Education({ setHideHeader }) {
 	useEffect(() => {
-setHideHeader(false);
+		setHideHeader(false);
 	}, [setHideHeader]);
 
 	const [contentSrc, setContentSrc] = useState('');
-	const [contentName, setContentName]
+	const [contentName, setContentName] = useState('');
 }
